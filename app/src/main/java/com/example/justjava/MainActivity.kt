a085638ff52ce8b61a4210d3f58e3a89028da7f9
Nikolay <niko.lapdgot@gmail.com>
2021-05-13 13:38:34 +0200

    Changes ( making the buttons more automated)

--- app/src/main/java/com/example/justjava/MainActivity.kt
+++ app/src/main/java/com/example/justjava/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.justjava
 
 
-import java.text.NumberFormat
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.text.NumberFormat
 
 
 /**
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     var quantity = 2;
     fun increment(view: View?) {
         quantity = quantity + 1;
@@ -32,7 +33,11 @@ class MainActivity : AppCompatActivity() {
      * This method is called when the order button is clicked.
      */
     fun submitOrder(view: View?) {
-        displayPrice(quantity * 5);
+        val int = quantity * 5;
+        val prices = "Total: $" + (int);
+        displayMessage(prices);
+        val niceMessage = prices + "\nthankyou!";
+        displayMessage(niceMessage);
     }
 
     /**
@@ -52,4 +57,13 @@ class MainActivity : AppCompatActivity() {
         val priceTextView = findViewById<View>(R.id.price_text_view) as TextView
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number))
     }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private fun displayMessage(message: String) {
+        val priceTextView = findViewById<View>(R.id.price_text_view) as TextView
+        priceTextView.text = message
+    }
+
 }
